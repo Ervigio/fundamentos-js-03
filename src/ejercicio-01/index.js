af6048439f4160ed2c2910cdf8b6ff40f9eff099
Ervigio <129860308+Ervigio@users.noreply.github.com>
2023-10-31 13:40:37 +0000

    Realizar ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,6 @@
 // Escribe tu código aquí:
+//Dada una cadena de texto en minúsculas, utiliza el método `toUpperCase()` para convertirla en mayúsculas. Luego, imprime el resultado.
+
+let cadenaDeTexto = "Esta cadena de texto será convertida a mayúsculas"
+let cadenaDeTextoUpperCase = cadenaDeTexto.toUpperCase()
+console.log(cadenaDeTextoUpperCase)
